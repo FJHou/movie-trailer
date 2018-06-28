ae4a2b98a48b8cfef4ae5ef0ab1f4a1468ecc77d
houfujian <ApeLiftingIron@163.com>
2018-06-28 23:15:50 +0800

    数据库存储成功

--- server/database/init.js
+++ server/database/init.js
@@ -33,6 +33,18 @@ exports.connect = () => {
       } else {
         throw new Error('数据库挂了');
       }
-    })
+    });
+
+    mongoose.connection.once('open', () => {
+      const Dog = mongoose.model('Dog', { name: String });
+      const doga = new Dog({ name: '阿尔法'});
+
+      doga.save().then(() => {
+        console.log('go go go');
+      });
+
+      resolve();
+      console.log('MongoDB Connected Successfully');
+    });
   })
 }
